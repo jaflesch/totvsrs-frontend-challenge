7b5df4cc4ffb0bd7a2c86de9235af0715c2f4980
Lucas de Oliveira <lucasoligon@hotmail.com>
2020-10-15 23:59:57 -0300

    Adiciona troca simples de página com jquery

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,22 @@
-console.log("TOTVS RS")
+//console.log("TOTVS RS")
+
+//$(document).ready(function () {
+
+$(".btn").on('click', function (e) {
+    e.preventDefault();
+    var url = $(this).attr('href');
+    chamaPagina(url);
+});
+
+//chama página
+function chamaPagina(url) {
+    $.ajax(
+        {
+            type: 'get',
+            url: url,
+            dataType: 'text',
+            success: function (url) {
+                $(".conteudo").html(url);
+            }
+        });
+}
