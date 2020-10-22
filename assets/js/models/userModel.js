c5d03a12c38965199b5d9e95352fa8e6fa4d874b
Tiago <ottiagos@gmail.com>
2020-10-22 20:36:12 -0300

    Refactor models

--- assets/js/models/userModel.js
+++ assets/js/models/userModel.js
@@ -1,50 +1,10 @@
 class UserModel {
 
-    constructor() {
-       this.store = localStorage;
-    }   
+    constructor(id = null, nome = null, email = null, senha = null) {
+       this.id = id;
+       this.nome = nome;
+       this.email = email;
+       this.senha = senha;
+    }  
 
-    //Salva usuário no localstorage
-    salvaUser(nome, email , password) {
-
-        let usuariosDB = this.getUsers();
-        let arrayUsuarios = [];
-        let newID = 1;
-
-        if(usuariosDB){
-            arrayUsuarios = JSON.parse(usuariosDB);
-            newID = parseInt(arrayUsuarios.length) + 1;
-        }
-
-        let usuario  = {'id' : newID, 'nome': nome, 'email' : email, 'senha' : password};
-        arrayUsuarios.push(usuario);
-
-        //Salva no localstorage
-        this.store.setItem('usuarios', JSON.stringify(arrayUsuarios));
-
-        //Salva como usuário logado
-        this.setLogado(newID, nome);
-        return this.getLogado();
-    }
-
-    //Salva os dados do usuário logado
-    setLogado(id,nome) {
-        this.store.setItem('logado', JSON.stringify({'id' : id, 'nome' : nome}));
-    }
-
-    //Retorna o usuário se estiver logado
-    getLogado() {
-        return this.store.getItem('logado');
-    }
-
-    //Retorna os usuários salvos
-    getUsers() {
-        return this.store.getItem('usuarios');
-    }
-    //Faz loggout removendo a key:value de logado
-    logout (){
-        this.store.removeItem('logado');
-    }
 }   
-
-const userModel = new UserModel();
