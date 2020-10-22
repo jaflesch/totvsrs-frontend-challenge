72479fb1f48131442b82ab501cfac92d3bfe12a4
Tiago <ottiagos@gmail.com>
2020-10-22 20:37:52 -0300

    View Edit e Login modificadas

---
+++ assets/js/views/viewEditAtividade.js
@@ -0,0 +1,28 @@
+const ViewEditAtividade = (titulo, descricao , date , status) => {
+
+
+    return(
+    `<div class="viewBase modalAtividade clearfix">
+            <h2>Edite os dados de sua atividade</h2>
+            <form>
+                <input type="text" value="${titulo}" name="titulo" placeholder="Digite o título da atividade">
+                <textarea name="descricao" rows="4" cols="50">${descricao}</textarea>
+    
+                <div class="formRow clearfix">
+                    <input name="date" type="date" value="${date}">
+                    <select name="status">
+                        <option value="0" ${status === "0" ? "selected" : ""}>Backlog</option>
+                        <option value="1" ${status === "1" ? "selected" : ""}>Em Andamento</option>
+                        <option value="2" ${status === "2" ? "selected" : ""}>Finalizado</option>
+                    </select>
+                </div>
+                
+                <button class="btn btn-solid btn-cancelar">Cancelar</button>
+                <button class="btn btn-solid btn-salvar">Salvar</button>
+                <button class="btn btn-solid btn-excluir">Excluir</button>
+            </form>
+        </div>`
+    );
+}
+
+    
--- assets/js/views/viewLogin.js
+++ assets/js/views/viewLogin.js
@@ -10,4 +10,3 @@ const ViewLogin = () => (
     </div>`
 );
 
-    
