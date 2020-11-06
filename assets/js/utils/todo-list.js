b9d9825422b45ae21ce09b96a7cd39fe8d4b96f7
dev01dwu <diegoferreirax95@gmail.com>
2020-11-06 13:35:25 -0300

    Cadastro item - inicio 12:20

--- assets/js/utils/todo-list.js
+++ assets/js/utils/todo-list.js
@@ -1,3 +1,5 @@
+var userData = {};
+
 function renderTodoListItensByUserId(id) {
     let lsDataTodoList = getItemCache('lsDataTodoList')['data'];
 
@@ -18,12 +20,42 @@ function renderTodoListItensByUserId(id) {
 
 function registerTodoItem() {
 
+    let lsDataTodoList = getItemCache('lsDataTodoList')['data'];
+
+    let filterDataTodoListUser = lsDataTodoList.filter(
+        item => item.userId === userData.id
+    );
+
+    let nextIdTodoItem = filterDataTodoListUser.length === 0 ? 1 : filterDataTodoListUser[filterDataTodoListUser.length - 1].id + 1;
+
+    let objTodoItem = {
+        id: nextIdTodoItem,
+        userId: userData.id,
+
+        titulo: document.getElementById('titleTodoItem').value,
+        descricao: document.getElementById('descriptionTodoItem').value,
+        data: document.getElementById('dateTodoItem').value,
+        status: parseInt(document.getElementById('statusTodoItem').value)
+    };
+
+    lsDataTodoList.push(objTodoItem);
+
+    removeItemCache('lsDataTodoList');
+    setItemCache('lsDataTodoList', lsDataTodoList);
+
+    renderTodoListItensByUserId(userData.id);
+
+    setTimeout(() => {
+        resetRegisterTodoItem();
+    }, 100);
 }
 
 function renderInfoUser(filterUserLogin) {
-    document.getElementById('desc-name-user').innerHTML = 'Nome: ' + filterUserLogin[0].nome;
-    document.getElementById('desc-email-user').innerHTML = 'Email: ' + filterUserLogin[0].email;
-    document.getElementById('desc-id-user').innerHTML = 'Id: ' + filterUserLogin[0].id;
+    userData = filterUserLogin[0];
+
+    document.getElementById('desc-name-user').innerHTML = 'Nome: ' + userData.nome;
+    document.getElementById('desc-email-user').innerHTML = 'Email: ' + userData.email;
+    document.getElementById('desc-id-user').innerHTML = 'Id: ' + userData.id;
 }
 
 function resetInfoUser() {
@@ -34,6 +66,14 @@ function resetInfoUser() {
 
 function resetTodoList() {
     document.getElementById('render-todo-list').innerHTML = '';
+}
+
+function resetRegisterTodoItem() {
+    document.getElementById('titleTodoItem').value = '';
+    document.getElementById('descriptionTodoItem').value = '';
+    document.getElementById('dateTodoItem').value = '';
+    document.getElementById('statusTodoItem').value = '0';
+    document.getElementById('closeModalRegisterTodoItem').click();
 }
 
 
