a4aab93503c592e47793ae0663711435ec700299
Luan Medeiros Silveira <luanmedeirossilveira@gmail.com>
2020-12-13 16:43:38 -0300

    Criação Todolist
    Implementação do component todolist e também começando a estilização dela.

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,6 +6,7 @@
         const router = new Router([
             new Route('login', 'login.html', true),
             new Route('register', 'register.html'),
+            new Route('todolist', 'todolist.html'),
         ]);
     }
     init();
