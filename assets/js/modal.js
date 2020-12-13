9f64d5eb6ee511498daa6444af0fa56d8874249d
Luan Medeiros Silveira <luanmedeirossilveira@gmail.com>
2020-12-13 17:45:43 -0300

    Funcionalidade Modal
    Iniciei a funcionalidade do modal para que a pessoa possa ter acesso a edição de uma linha da tabela, clique sobre o titulo.

---
+++ assets/js/modal.js
@@ -0,0 +1,19 @@
+//Função para abrir o modal
+function openModal(){
+    document.getElementById("fade").style.display = "flex";
+}
+
+//Função para fechar o modal
+function closeModal(){
+    document.getElementById("fade").style.display = "none"; 
+}
+
+//Função para excluir um item do modal
+function deleteModal(){
+    console.log("item deletado!");
+}
+
+//Função para salvar um item do modal
+function saveModal(){
+    console.log("item salvo!");
+}
