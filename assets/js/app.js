b69011294d7aa682b56eaf8378b6acd09e5b62e5
danieljustino79@gmail.com <Ulbrati2000>
2020-12-28 10:34:29 -0300

    Validação básica do form de autenticação.

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,9 @@ localStorage.setItem("page", "home");
 if (localStorage.getItem("page") == "home"){
 	$("#body-dynamic").loadTemplate("views/login.html");
 }
+
+function isUndefinedOrEmpty(text){
+    if(text === undefined || text == null || text.trim() === '')
+        return true;
+    return false;
+}
--- assets/js/login.js
+++ assets/js/login.js
@@ -1,5 +1,44 @@
 $('#btnAcessar').click(function(e){
-	e.preventDefault();
-	
-	$("#body-dynamic").loadTemplate("views/list.html");
+    e.preventDefault();
+
+    $('#emailHelp').addClass('d-none');
+    $('#senhaHelp').addClass('d-none');
+    
+    var validation = {hasError: false, fields:[]};
+    var email = $('#email');
+    var senha = $('#senha');
+
+    if(isUndefinedOrEmpty(email.val())){
+        validation.hasError = true;
+        validation.fields['email'] = 'Email obrigatório.';
+    }
+    else if(!validateEmail(email.val())){
+        validation.hasError = true;
+        validation.fields['email'] = 'Email incorreto.';
+    }
+    
+    if(isUndefinedOrEmpty(senha.val())){
+        validation.hasError = true;
+        validation.fields['senha'] = 'Senha obrigatória.';
+    }
+
+    if(validation.hasError){
+        if(!isUndefinedOrEmpty(validation.fields['email'])){
+            $('#emailHelp').html(validation.fields['email']);
+            $('#emailHelp').removeClass('d-none');
+        }
+
+        if(!isUndefinedOrEmpty(validation.fields['senha'])){
+            $('#senhaHelp').html(validation.fields['senha']);
+            $('#senhaHelp').removeClass('d-none');
+        }
+    }
+    else{
+        $('#body-dynamic').loadTemplate('views/list.html');
+    }
 });
+
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
