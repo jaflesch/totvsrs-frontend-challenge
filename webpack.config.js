b375ea439758c5b8a28c6f621cf44c97e3fbc694
AndreMoura <andre.moura@kinghost.com.br>
2021-01-03 11:27:43 -0300

    Criado estilo para botoes e inputs

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: "./src/main.js",
@@ -11,6 +12,9 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./index.html",
     }),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css'
+    })
   ],
   module: {
   rules: [
@@ -18,6 +22,14 @@ module.exports = {
       test: /\.html$/i,
       loader: "html-loader",
     },
+    {
+      test: /\.(sa|sc|c)ss$/,
+      use: [
+        MiniCssExtractPlugin.loader,
+        'css-loader',
+        'sass-loader'
+      ]
+    }
   ],
   },
   devServer: {
