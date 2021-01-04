f89c0c938d6dd1d20f89ca1a52a230a8ee0cdbb5
AndreMoura <andre.moura@kinghost.com.br>
2021-01-03 23:25:45 -0300

    Melhorado layout da pagina inicial de todos

--- src/controllers/TodoViewController.js
+++ src/controllers/TodoViewController.js
@@ -3,7 +3,7 @@ import UpdateTodoFormController from '../controllers/UpdateTodoFormController'
 import CreateTodoFormController from '../controllers/CreateTodoFormController';
 import ListTodoService from '../services/ListTodoService'
 import AuthenticateUserService from '../services/AuthenticateUserService'
-
+import svgIcon from '../assets/images/logoutIcon.svg';
 class TodoViewController {
     constructor() {
         this.listTodoService = new ListTodoService();
@@ -14,7 +14,9 @@ class TodoViewController {
         if(user){
             const element = document.createElement('div');
             element.innerHTML = todoHomeView;
+            element.id = 'todoHome';
             rootContainer.appendChild(element);
+
             await this.loadTodos(user.id)
             this._handleEndSession();
             this._handleOpenCreateTodoModal();
@@ -31,14 +33,17 @@ class TodoViewController {
     }
     async _todosMount(todo) {
         const updateTodoForm = new UpdateTodoFormController();
-        todosContainer.innerHTML = '';
+        todosContainer.innerHTML = ''
+        this._mountTodoTable();
         const todoCard = todo.forEach(element => {
             
-            const container = document.createElement('div');
-            const title = document.createElement('button');
-            const description = document.createElement('p');
-            const status = document.createElement('span');
-            const date = document.createElement('span');
+
+            const container = document.createElement('tr');
+            
+            const id = document.createElement('td');
+            const title = document.createElement('td');
+            const status = document.createElement('td');
+            const date = document.createElement('td');
 
             container.setAttribute('id', element.id);
             container.setAttribute('class', 'todoCard');
@@ -47,23 +52,43 @@ class TodoViewController {
             title.addEventListener('click', () => {
                 updateTodoForm.create(element.id)
             })
-            description.innerText = element.description;
+            id.innerText = element.id;
             status.innerText = element.status;
             date.innerText = element.date;
-            
-            
 
-            container.append(title, description, status, date);
+            container.append(id, title, status, date);
             todosContainer.appendChild(container);
-
             });
         return todoCard;
     }
+    _mountTodoTable() {
+        const container = document.createElement('tr');
+        const id = document.createElement('th');
+        const title = document.createElement('th');
+        const status = document.createElement('th');
+        const date = document.createElement('th');
+
+        id.innerHTML = 'ID';
+        title.innerHTML = 'Titulo';
+        status.innerHTML = 'Status';
+        date.innerHTML = 'Data';
+
+        container.append(id, title, status, date);
+        return todosContainer.appendChild(container);
+        
+    }
     _handleEndSession() {
         const authenticateUserService = new AuthenticateUserService()
+        const buttonEndSession = document.createElement('button');
+        const endSessionIcon = document.createElement('img');
+        endSessionIcon.src = svgIcon;
+        buttonEndSession.id = 'endSession';
+        buttonEndSession.appendChild(endSessionIcon);
+        applicationHeader.appendChild(buttonEndSession);
         endSession.addEventListener('click', element => {
             authenticateUserService.remove();
             window.location.hash = "#";
+            applicationHeader.removeChild(buttonEndSession);
             return element;
         })
     }
