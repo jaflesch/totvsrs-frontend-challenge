5f5d37d9bd17aa780e63a59a541982fa676d6522
Arthur Oliveira <abol2109@gmail.com>
2021-02-04 17:54:56 -0300

    Started working  - inicial page created
    The html's skeleton for the Start Page has been created.
    The function for change "pages" without refresing (SPA) has been created.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,20 @@
-console.log("TOTVS RS")
+window.onload = function() {
+    document.getElementById("startPage").hidden = false;
+    document.getElementById("todolist").hidden = true;
+
+}
+
+
+function validateLogin() {
+    window.alert(sessionStorage.getItem("email"));
+    var elemento = document.getElementById("email");
+    sessionStorage.setItem("email", elemento.value);
+
+    document.getElementById("startPage").hidden = true;
+    document.getElementById("todolist").hidden = false;
+}
+
+function goToCreateLogin() {
+    document.getElementById("startPage").hidden = true;
+    //mostrar elemento de login criado
+}
