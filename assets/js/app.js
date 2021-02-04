8051976e2879bb051088545a09de3fe5d506e424
Arthur Oliveira <abol2109@gmail.com>
2021-02-04 18:57:50 -0300

    Created the skeleton for creating a new login
    int the index.html file renamed id and onclick function

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,6 @@ window.onload = function() {
 
 }
 
-
 function validateLogin() {
     window.alert(sessionStorage.getItem("email"));
     var elemento = document.getElementById("email");
@@ -12,9 +11,46 @@ function validateLogin() {
 
     document.getElementById("startPage").hidden = true;
     document.getElementById("todolist").hidden = false;
+
 }
 
-function goToCreateLogin() {
-    document.getElementById("startPage").hidden = true;
-    //mostrar elemento de login criado
+function createLogin() {
+
+    //get all the users
+    var array = JSON.parse((sessionStorage.getItem("allUsers")));
+    if (array == null) {
+        array = new Array();
+    }
+    console.log(array);
+
+    //create the new user
+    obj = new Object();
+    obj = {
+        email: document.getElementById("email").value,
+        password: document.getElementById("password").value
+    };
+
+    //checks if already exists an account with this email
+    if (containsEmail(obj, array)) {
+        window.alert("Email já existe");
+    } else {
+        console.log("NAO inclui");
+
+        //insert new user into user's array
+        array.push(obj);
+
+        //save user's array
+        sessionStorage.setItem("allUsers", JSON.stringify(array));
+
+        document.getElementById("startPage").hidden = true;
+    }
+}
+
+function containsEmail(obj, array) {
+    for (var i = 0; i < array.length; i++) {
+        if (array[i].email == obj.email) {
+            return true;
+        }
+    }
+    return false;
 }
