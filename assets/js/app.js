9f771a2ff6729acbf7ccbf25e58fb7a3dcc6884f
Arthur Oliveira <abol2109@gmail.com>
2021-02-04 19:33:07 -0300

    Created the function for the user to log in
    and chenged the "containsEmail" function for the "returnEmailPosition" function

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,13 +5,36 @@ window.onload = function() {
 }
 
 function validateLogin() {
-    window.alert(sessionStorage.getItem("email"));
-    var elemento = document.getElementById("email");
-    sessionStorage.setItem("email", elemento.value);
 
-    document.getElementById("startPage").hidden = true;
-    document.getElementById("todolist").hidden = false;
+    //get all the users
+    var array = JSON.parse((sessionStorage.getItem("allUsers")));
+    if (array == null) {
+        array = new Array();
+    }
+    console.log(array);
 
+    //create the user object
+    obj = new Object();
+    obj = {
+        email: document.getElementById("email").value,
+        password: document.getElementById("password").value
+    };
+
+    var emailPos = returnEmailPosition(obj, array);
+
+    if (emailPos == -1) {
+        window.alert("Conta não existente");
+        return;
+    }
+
+    if (obj.password == array[emailPos].password) {
+        document.getElementById("startPage").hidden = true;
+        document.getElementById("todolist").hidden = false;
+
+        sessionStorage.setItem("activeUser", emailPos);
+    } else {
+        window.alert("senha incorreta");
+    }
 }
 
 function createLogin() {
@@ -31,11 +54,7 @@ function createLogin() {
     };
 
     //checks if already exists an account with this email
-    if (containsEmail(obj, array)) {
-        window.alert("Email já existe");
-    } else {
-        console.log("NAO inclui");
-
+    if (returnEmailPosition(obj, array) == -1) {
         //insert new user into user's array
         array.push(obj);
 
@@ -43,14 +62,17 @@ function createLogin() {
         sessionStorage.setItem("allUsers", JSON.stringify(array));
 
         document.getElementById("startPage").hidden = true;
+    } else {
+        window.alert("Email já existe");
     }
 }
 
-function containsEmail(obj, array) {
+function returnEmailPosition(obj, array) {
     for (var i = 0; i < array.length; i++) {
         if (array[i].email == obj.email) {
-            return true;
+            return i;
         }
     }
-    return false;
+    //dont exist
+    return -1;
 }
