eea18128fd5ba4100cc120bca82f8e06c716230b
Guilherme Doval <doval.guilherme94@gmail.com>
2021-02-18 19:08:31 -0300

    consertado problemas de linting

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,24 @@
-
-
 window.onload = () => {
   loadPage('signUp')
 }
 
-function loadPage (pageName) {
-  xhr = new XMLHttpRequest();
+function loadPage(pageName) {
+  const xhr = new XMLHttpRequest()
 
   const contentContainer = document.getElementById('contentContainer')
 
-  xhr.onload = function() {
+  xhr.onload = function () {
     if (this.status === 200) {
       contentContainer.innerHTML = xhr.responseText
       switch (pageName) {
         case 'signUp':
-        setupSignUpForm()
-        break
+          setupSignUpForm()
+          break
         case 'signIn':
-        setupSignInForm()
-        break
+          setupSignInForm()
+          break
         case 'dashboard':
-        setupTodos()
-
+          setupTodos()
       }
     } else {
       console.warn('Error loading page content')
@@ -29,81 +26,76 @@ function loadPage (pageName) {
   }
   xhr.open('GET', `../../src/views/${pageName}.html`)
   xhr.send()
-
 }
+function setupSignUpForm() {
+  console.log('Visitando página de Registro')
 
-
-function setupSignUpForm () {
   const signUpForm = {
     form: document.getElementById('signUpForm'),
-    userName : document.getElementById('userNameInput'),
-    userEmail : document.getElementById('userEmailInput'),
-    userPassword : document.getElementById('userPasswordInput'),
+    userName: document.getElementById('userNameInput'),
+    userEmail: document.getElementById('userEmailInput'),
+    userPassword: document.getElementById('userPasswordInput')
   }
 
-  signUpForm.form.addEventListener('submit', (event) => {
+  signUpForm.form.addEventListener('submit', event => {
     event.preventDefault()
-    handleCreateUser( signUpForm )
+    handleCreateUser(signUpForm)
   })
 
-  document.getElementById('logInPageLink').addEventListener('click', (event) => {
+  document.getElementById('logInPageLink').addEventListener('click', event => {
     event.preventDefault()
     loadPage('signIn')
   })
-
 }
 
-function setupSignInForm ( ) {
+function setupSignInForm() {
+  console.log('Visitando página de SignIn')
   const signInForm = {
     form: document.getElementById('signInForm'),
-    userEmail : document.getElementById('userEmailInput'),
-    userPassword : document.getElementById('userPasswordInput'),
+    userEmail: document.getElementById('userEmailInput'),
+    userPassword: document.getElementById('userPasswordInput')
   }
 
-  document.getElementById('signUpPageLink').addEventListener('click', (event) => {
+  signInForm.form.addEventListener('submit', event => {
+    event.preventDefault()
+    handleLoginUser(signInForm)
+  })
+
+  document.getElementById('signUpPageLink').addEventListener('click', event => {
     event.preventDefault()
     loadPage('signUp')
   })
 }
 
-function handleCreateUser ( {userName, userEmail, userPassword} ) {
-  let users = new Array();
-  users = JSON.parse(sessionStorage.getItem("users"))
+function handleCreateUser({ userName, userEmail, userPassword }) {
+  let users = []
+  users = JSON.parse(sessionStorage.getItem('users'))
 
-  if (!users ) {
+  if (!users) {
     const user = {
       id: 1,
-      userName : userName.value,
-      userEmail : userEmail.value,
-      userPassword : userPassword.value,
+      userName: userName.value,
+      userEmail: userEmail.value,
+      userPassword: userPassword.value
     }
 
     users = new Array(user)
 
-    sessionStorage.setItem("users",JSON.stringify(users))
+    sessionStorage.setItem('users', JSON.stringify(users))
   } else {
-
     if (users.find(user => user.userEmail === userEmail.value)) {
       alert('Esse email já está registrado')
-
     } else {
       const user = {
-        id: users.length+1,
-        userName : userName.value,
-        userEmail : userEmail.value,
-        userPassword : userPassword.value,
+        id: users.length + 1,
+        userName: userName.value,
+        userEmail: userEmail.value,
+        userPassword: userPassword.value
       }
-      users.push(user);
-      sessionStorage.setItem("users",JSON.stringify(users))
-
-
+      users.push(user)
+      sessionStorage.setItem('users', JSON.stringify(users))
     }
   }
-
-
-
 }
 
-function handleLoginUser ({userEmail, userPassword}) {
-
-}
+function handleLoginUser({ userEmail, userPassword }) {}
