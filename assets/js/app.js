b884f4e4d23526f069c4242209457b3a901130e2
Guilherme Doval <doval.guilherme94@gmail.com>
2021-02-18 16:13:24 -0300

    estilização da página de sign in / criação de login de usuário

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,9 @@
-let authStatus
 
 window.onload = () => {
-  loadPage('signUp');
+   loadPage('signUp')
 }
 
-function loadPage (pageName) {
+async function loadPage (pageName) {
   xhr = new XMLHttpRequest();
 
   const contentContainer = document.getElementById('contentContainer')
@@ -12,21 +11,53 @@ function loadPage (pageName) {
   xhr.onload = function() {
     if (this.status === 200) {
       contentContainer.innerHTML = xhr.responseText
+      switch (pageName) {
+        case 'signUp':
+        setupSignUpForm()
+        break
+        case 'signIn':
+        setupSignInForm()
+        case 'dashboard':
+        setupTodos()
+
+      }
     } else {
       console.warn('Error loading page content')
     }
   }
   xhr.open('GET', `../../src/views/${pageName}.html`)
   xhr.send()
+
 }
 
 
 function setupSignUpForm () {
-  const signUpForm = document.querySelector('#signUpForm')
-  signUpForm.addEventListener('submit', handleCreateAccount)
+  const signUpForm = {
+    form: document.getElementById('signUpForm'),
+    userName : document.getElementById('userNameInput'),
+    userEmail : document.getElementById('userEmailInput'),
+    userPassword : document.getElementById('userPasswordInput'),
+    submit : document.getElementById('submitButton'),
+  }
+
+  signUpForm.form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    handleCreateUser( signUpForm )
+  })
+
+  console.log(signUpForm)
 }
 
-function handleCreateAccount () {
+function handleCreateUser ( signUpForm ) {
+  function validateData () {
 
+  }
+  const createdUser = {
+    userName:  signUpForm.userName.value,
+    userEmail: signUpForm.userEmail.value,
+    userPassword: signUpForm.userPassword.value,
+  }
 
+  console.log(createdUser)
 }
+
