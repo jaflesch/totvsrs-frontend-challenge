99d155eef3782d5bb65e5e0dcae729b1bec344fe
Guilherme Doval <doval.guilherme94@gmail.com>
2021-02-18 12:01:49 -0300

    carregamento de pagina via ajax

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,32 @@
+let authStatus
 
+window.onload = () => {
+  loadPage('signUp');
+}
+
+function loadPage (pageName) {
+  xhr = new XMLHttpRequest();
+
+  const contentContainer = document.getElementById('contentContainer')
+
+  xhr.onload = function() {
+    if (this.status === 200) {
+      contentContainer.innerHTML = xhr.responseText
+    } else {
+      console.warn('Error loading page content')
+    }
+  }
+  xhr.open('GET', `../../src/views/${pageName}.html`)
+  xhr.send()
+}
+
+
+function setupSignUpForm () {
+  const signUpForm = document.querySelector('#signUpForm')
+  signUpForm.addEventListener('submit', handleCreateAccount)
+}
+
+function handleCreateAccount () {
+
+
+}
