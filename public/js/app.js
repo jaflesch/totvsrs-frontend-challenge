3f23d5d03a69b0b1d252b60289604c0d0b6abe51
Guilherme Doval <doval.guilherme94@gmail.com>
2021-02-19 16:08:42 -0300

    populando tela com todos provisórios / implementado criação de todos

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,43 @@
+// Math.random().toString().substr(2, 8);
+
 window.onload = () => {
-  loadPage('dashboard')
+  loadPage('signIn')
+  const todos = [
+    {
+      id: 42347210,
+      userId: 1,
+      title: 'Teste 01',
+      description: 'Lorem ipsum dolor sit amet, consectetur adip',
+      date: new Date(),
+      status: 1
+    },
+    {
+      id: 42347210,
+      userId: 2,
+      title: 'Teste 02',
+      description: 'Lorem ipsum dolor sit amet, consectetur adip',
+      date: new Date(),
+      status: 1
+    },
+    {
+      id: 42347210,
+      userId: 1,
+      title: 'Teste 03',
+      description: 'Lorem ipsum dolor sit amet, consectetur adip',
+      date: new Date(),
+      status: 1
+    },
+    {
+      id: 42347210,
+      userId: 1,
+      title: 'Teste 03',
+      description: 'Lorem ipsum dolor sit amet, consectetur adip',
+      date: new Date(),
+      status: 0
+    }
+  ]
+
+  sessionStorage.setItem('todos', JSON.stringify(todos))
 }
 
 function loadPage(pageName) {
@@ -18,7 +56,7 @@ function loadPage(pageName) {
           setupSignInForm()
           break
         case 'dashboard':
-          setupTodos()
+          setupDashboard()
       }
     } else {
       console.warn('Error loading page content')
@@ -66,6 +104,23 @@ function setupSignInForm() {
     event.preventDefault()
     loadPage('signUp')
   })
+}
+
+function setupDashboard() {
+  console.log('Visitando dashboard')
+
+  const authenticatedUser = JSON.parse(
+    sessionStorage.getItem('authenticatedUser')
+  )
+
+  const tableRef = document
+    .getElementById('todosTable')
+    .getElementsByTagName('tbody')[0]
+
+  const addTodoButton = document.getElementById('addTodoButton')
+  addTodoButton.addEventListener('click', handleCreateTodoModal)
+
+  loadTodos(authenticatedUser, tableRef)
 }
 
 function handleCreateUser({ userName, userEmail, userPassword }) {
@@ -132,4 +187,69 @@ function handleLoginUser({ userEmail, userPassword }) {
   } else {
     alert('Não foi encontrado um usuário com o e-mail fornecido')
   }
+}
+
+function handleCreateTodo({ todoTitle, todoStatuses }) {}
+
+function loadTodos(authenticatedUser, table) {
+  let todos = []
+  todos = JSON.parse(sessionStorage.getItem('todos'))
+  let userTodos = []
+  table.innerHTML = ''
+
+  if (todos) {
+    userTodos = todos.filter(todo => todo.userId === authenticatedUser.id)
+    for (let i = 0; i < userTodos.length; i++) {
+      let parsedStatus
+      switch (userTodos[i].status) {
+        case 0:
+          parsedStatus = 'Backlog'
+          break
+        case 1:
+          parsedStatus = 'Em andamento'
+          break
+        case 2:
+          parsedStatus = 'Finalizado'
+          break
+      }
+
+      const todoRow = `<tr>
+                        <td>${userTodos[i].id}</td>
+                        <td><a href="" id="${userTodos[i].id}">${userTodos[i].title}</a></td>
+                        <td>${userTodos[i].date}</td>
+                        <td>${parsedStatus}</td>
+                      </tr>`
+      table.innerHTML += todoRow
+    }
+    const todosTitleArray = document.querySelectorAll('td a')
+    for (let i = 0; i < todosTitleArray.length; i++) {
+      todosTitleArray[i].addEventListener('click', e => {
+        e.preventDefault()
+        console.log(e.currentTarget.id)
+      })
+    }
+  }
+
+  console.log(userTodos)
+}
+
+function handleCreateTodoModal() {
+  const todoModal = document.getElementById('createTodoModalContainer')
+  const closeModalButton = document.getElementById('cancelCreateButton')
+  todoModal.classList.add('active')
+
+  const createTodoForm = {
+    form: document.getElementById('createTodoForm'),
+    todoTitle: document.querySelector('#createTodoForm #todoTitle'),
+    todoStatuses: document.getElementsByName('createTodoStatus')
+  }
+
+  createTodoForm.form.addEventListener('submit', event => {
+    event.preventDefault()
+    handleCreateTodo(createTodoForm)
+  })
+
+  closeModalButton.addEventListener('click', function () {
+    todoModal.classList.remove('active')
+  })
 }
